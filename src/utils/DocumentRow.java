b8c143e17186a357e19b9d61108478d4d4b297a3
orris27 <gsorris27@gmail.com>
2019-01-21 23:03:40 +0800

    change place2

---
+++ src/utils/DocumentRow.java
@@ -0,0 +1,56 @@
+package utils;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class DocumentRow {
+
+    private final IntegerProperty id;
+    private final StringProperty creator;
+    private final StringProperty title;
+
+    public DocumentRow(int id, String creator, String title) {
+        this.id = new SimpleIntegerProperty(id);
+        this.creator = new SimpleStringProperty(creator);
+        this.title = new SimpleStringProperty(title);
+    }
+
+    public String getCreator() {
+        return creator.get();
+    }
+
+    public void setCreator(String creator) {
+        this.creator.set(creator);
+    }
+
+    public StringProperty creatorProperty() {
+        return creator;
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
+    }
+
+    public StringProperty titleProperty() {
+        return title;
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+}
